92aab63e660961178a6eb3d7e9094fd03a8ebe6d
Abderraouf Benbellat <rbenballat@gmail.com>
2023-11-05 19:49:16 +0100

    create reelsView

---
+++ lib/featurs/Reels/prisentation/view/ReelsView.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class ReelsView extends StatelessWidget {
+  const ReelsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
