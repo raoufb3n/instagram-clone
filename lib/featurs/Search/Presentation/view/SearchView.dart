0a1af8a159769dd86d23a251aed39d3c1949ea61
Abderraouf Benbellat <rbenballat@gmail.com>
2023-11-05 19:45:22 +0100

    create Search View

---
+++ lib/featurs/Search/Presentation/view/SearchView.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
