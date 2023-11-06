0402901bf117deea411cea8626d1fcc5a0d20a4d
Abderraouf Benbellat <rbenballat@gmail.com>
2023-11-06 19:16:17 +0100

    made postLayout

---
+++ lib/featurs/Search/Presentation/view/widgets/PostLayout.dart
@@ -0,0 +1,87 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/featurs/Search/Presentation/view/widgets/SearchViewBody.dart';
+
+class PostLayout extends StatelessWidget {
+  const PostLayout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 3.3,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 1.5,
+                child: GridView.count(
+                  physics: NeverScrollableScrollPhysics(),
+                  padding: EdgeInsets.only(right: 1.5),
+                  crossAxisSpacing: 1.5,
+                  mainAxisSpacing: 1.5,
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  children: [
+                    SearchImageWidget(),
+                    SearchImageWidget(),
+                    SearchImageWidget(),
+                    SearchImageWidget(),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height / 3.1,
+                  child: Image.asset(
+                    'assets/images/nigel-hoare-_r3nclhPoPM-unsplash.jpg',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 3.3,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height / 3.1,
+                  child: Image.asset(
+                    'assets/images/nigel-hoare-_r3nclhPoPM-unsplash.jpg',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 1.5,
+                child: GridView.count(
+                  physics: NeverScrollableScrollPhysics(),
+                  padding: EdgeInsets.only(left: 1.5),
+                  crossAxisSpacing: 1.5,
+                  mainAxisSpacing: 1.5,
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  children: [
+                    SearchImageWidget(),
+                    SearchImageWidget(),
+                    SearchImageWidget(),
+                    SearchImageWidget(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/featurs/Search/Presentation/view/widgets/SearchViewBody.dart
+++ lib/featurs/Search/Presentation/view/widgets/SearchViewBody.dart
@@ -1,35 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/featurs/Search/Presentation/view/widgets/PostLayout.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            SizedBox(
-             width: 300,
-             height: MediaQuery.of(context).size.height /3,
-             
-              child: GridView.count(
-                shrinkWrap: true,
-                crossAxisCount: 2,
-                children: [
-                  SearchImageWidget(),
-                  SearchImageWidget(),
-                  SearchImageWidget(),
-                  SearchImageWidget(),
-                ],
-              ),
-            )
-          ],
-        )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [PostLayout(), PostLayout()],
+      ),
     );
   }
 }
+
+
 
 class SearchImageWidget extends StatelessWidget {
   const SearchImageWidget({
@@ -38,14 +23,11 @@ class SearchImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 40,
-      child: AspectRatio(
-        aspectRatio: 2 / 2,
-        child: Image.asset(
-          'assets/images/nigel-hoare-_r3nclhPoPM-unsplash.jpg',
-          fit: BoxFit.fill,
-        ),
+    return AspectRatio(
+      aspectRatio: 4 / 4,
+      child: Image.asset(
+        'assets/images/nigel-hoare-_r3nclhPoPM-unsplash.jpg',
+        fit: BoxFit.fill,
       ),
     );
   }
